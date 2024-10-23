34ec2acb46f88c1afa0ef98cf5f7f0b660d6403d
Praveenkumar K <praveenkrishh01@gmail.com>
2024-10-23 10:43:36 +0530

    = added

--- src/Components/userprofile.js
+++ src/Components/userprofile.js
@@ -8,7 +8,7 @@ export default function UserProfile(props){
     return(
         <>
         <h2>My name is {userinfo.name}, 
-            I'm {userinfo.age} years old{userinfo.location!=undefined?`, and I come from ${userinfo.location}`:null}.
+            I'm {userinfo.age} years old{userinfo.location!==undefined?`, and I come from ${userinfo.location}`:null}.
             User Status : {userinfo.isActive?`Active User`:<span>InActive User</span>}</h2>
         </>
     );
--- src/Components/userprofile2.js
+++ src/Components/userprofile2.js
@@ -5,7 +5,7 @@ export default function UserProfile2(props){
 
     return(
         <>
-        <h2>My name is {userinfo.name}, I'm a {userinfo.age >= 18 ? `Adult`:`Minor`}{userinfo.email!=undefined?`, and my email ID is ${userinfo.email}`:null}.</h2>
+        <h2>My name is {userinfo.name}, I'm a {userinfo.age >= 18 ? `Adult`:`Minor`}{userinfo.email!==undefined?`, and my email ID is ${userinfo.email}`:null}.</h2>
         </>
     );
 }
