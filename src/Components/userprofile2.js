8845ff64d166b43c1457599b715cff465031c8fb
Praveenkumar K <praveenkrishh01@gmail.com>
2024-10-24 11:25:12 +0530

    title_added

--- src/Components/userprofile2.js
+++ src/Components/userprofile2.js
@@ -5,7 +5,8 @@ export default function UserProfile2(props){
 
     return(
         <>
-        <h2>My name is {userinfo.name}, I'm a {userinfo.age >= 18 ? `Adult`:`Minor`}{userinfo.email && userinfo.name?`, and my email ID is ${userinfo.email}`:null}.</h2>
+        <h2>My name is {userinfo.name}, I'm a {userinfo.age >= 18 ? `Adult`:`Minor`}
+        {userinfo.email && userinfo.name?`, and my email ID is ${userinfo.email}`:null}.</h2>
         </>
     );
 }
